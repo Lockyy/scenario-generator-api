24e9b261e32c22d269b0b9890c4e367a5b2f6f03
Daniel Lockhart <daniel@lockyy.com>
2015-07-01 19:12:30 +0100

    Add static pages

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,7 +2,22 @@ class SiteController < ApplicationController
 
   layout 'site'
 
-  def index
+  def long
+  end
+
+  def short
+  end
+
+  def contact
+    @title = "Contact us"
+
+    render 'contact'
+  end
+
+  def support
+    @title = "Employee Support"
+
+    render 'contact'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'site#index'
+  root 'site#long'
+  get 'long', to: 'site#long'
+  get 'short', to: 'site#short'
+  get 'contact', to: 'site#contact'
+  get 'support', to: 'site#support'
 end
