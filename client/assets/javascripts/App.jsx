c2118beeb31c8fc44ec43a8219bb3630d17761a5
Halisson Vitor <halissonvit@gmail.com>
2015-07-02 19:18:20 -0300

    test a way to fetch the token on react

--- client/assets/javascripts/App.jsx
+++ client/assets/javascripts/App.jsx
@@ -4,10 +4,11 @@ import React from 'react';
 $(function onLoad() {
   function render() {
     if ($('#content').length > 0) {
+      var token = $('#content').data('authToken')
       React.render(
         <div>
           <div className='container'>
-              <h3 className='open-sans-light'>ReactJS Component</h3>
+              <h3 className='open-sans-light'>{token !== "" ? 'AuthToken: ' + token : 'Please login!'}</h3>
           </div>
         </div>,
         document.getElementById('content')
