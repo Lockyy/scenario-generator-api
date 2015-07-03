75eab9df82e0693caef49724c2d382b688f5dc45
Halisson Vitor <halissonvit@gmail.com>
2015-07-03 10:38:19 -0300

    add generate random password on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:yammer]
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, omniauth_providers: [:yammer]
 
   has_many :user_oauths, dependent: :destroy
   has_many :tokens, dependent: :destroy
+
+  def self.generate_password
+    Devise.friendly_token
+  end
 end
