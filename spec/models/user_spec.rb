22a6375232cafc63b25402c202f352a979e98956
Halisson Vitor <halissonvit@gmail.com>
2015-07-23 09:31:04 -0300

    create tag model and relationship with user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe User, type: :model do
   it { expect(subject).to validate_presence_of :name }
   it { expect(subject).to validate_presence_of :email }
+  it { expect(subject).to have_and_belong_to_many :tags }
 end
