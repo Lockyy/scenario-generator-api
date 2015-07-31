d79416a50b1844eb8de3525214002d720d3118cc
Halisson Vitor <halissonvit@gmail.com>
2015-07-31 15:35:13 -0300

    refactor(client) move review components to reviews folder

--- client/assets/javascripts/App.jsx
+++ client/assets/javascripts/App.jsx
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import React from 'react';
 import Dashboard from './components/Dashboard';
-import NewReviewPage from './components/products/NewReviewPage';
+import NewReviewPage from './components/reviews/NewReviewPage';
 import { Router, Route } from 'react-router';
 import { history } from 'react-router/lib/BrowserHistory';
 
