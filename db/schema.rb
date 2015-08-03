3d841bf78da4088201e044e66b3df3b05dbb8ed5
Daniel Lockhart <daniel@lockyy.com>
2015-08-03 21:48:39 +0100

    Add views columns to products table
    This will be used for comparing product popularity and will increment on every page view. We may need a rate limiter on this to prevent spamming to increase popularity.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722184133) do
+ActiveRecord::Schema.define(version: 20150803201614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20150722184133) do
     t.text     "description"
     t.string   "url"
     t.integer  "company_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "views",       default: 0
   end
 
   create_table "reviews", force: :cascade do |t|
