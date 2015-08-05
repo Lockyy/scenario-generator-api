3956547f0fb494065e0cf6679d1fd2015a938b93
Daniel Lockhart <daniel@lockyy.com>
2015-08-05 21:50:15 +0100

    Add 1-to-many relation between users and reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :reviewable, polymorphic: true
+  belongs_to :user
 
   # TODO: implement custom validator(validate presence of at least one of the fields)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   has_many :user_oauths, dependent: :destroy
   has_many :tokens, dependent: :destroy
+  has_many :reviews
   has_and_belongs_to_many :tags
 
   scope :with_oauth, ->(provider, uid) do
