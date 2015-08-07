c50b0bea41b6999f88b8c47fd65c0d54d67db238
Halisson Vitor <halissonvit@gmail.com>
2015-08-06 21:08:22 -0300

    feat(client) add validation error message to required fields on product creation

--- client/assets/javascripts/components/reviews/ProductFields.jsx
+++ client/assets/javascripts/components/reviews/ProductFields.jsx
@@ -13,18 +13,22 @@ const ProductFields  = React.createClass({
         <label htmlFor='product[company[name]]'>Company Name <span className='required'>*</span></label>
         <input type='text' className='form-control' placeholder='e.g. Microsoft' name='product[company[name]]'
           ref='product_company_name' required/>
+        <span className="help-block with-errors"></span>
       </div>
 
       <div className='form-group'>
         <label htmlFor='product[url]'>Product's website <span className='required'>*</span></label>
         <input type='text' className='form-control' placeholder='www.' name='product[url]'
-          pattern="[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)" title="Include a valid url" ref='product_url' required/>
+          pattern="[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+          title="Include a valid url" ref='product_url' required/>
+        <span className="help-block with-errors"></span>
       </div>
 
       <div className='form-group'>
         <label htmlFor='product[description]'>Description <span className='required'>*</span></label>
         <textarea type='text' className='form-control' placeholder='Write a brief description of the product'
           name='product[description]' rows='10' ref='product_description' required/>
+        <span className="help-block with-errors"></span>
       </div>
     </fieldset>);
   },
@@ -44,6 +48,7 @@ const ProductFields  = React.createClass({
               <button className="btn btn-default" type="button" disabled={true}>Go</button>
             </span>
           </div>
+          <span className="help-block with-errors"></span>
         </div>
 
         {newProduct ? this._getNewProductFields() : ''}
