7d4e42006218109865237c55b4695fbc949e665b
Daniel Lockhart <daniel@lockyy.com>
2015-08-07 20:21:33 +0100

    feature(api) Prevent duplicate products on dashboard.
    Keep a list of the IDs that are used when creating a section of the Dashboard in Fletcher::Dashboard. Use this to exclude results from later sections.

--- app/services/fletcher/dashboard.rb
+++ app/services/fletcher/dashboard.rb
@@ -13,21 +13,26 @@ module Fletcher
 
     def initialize(params = {})
       @params = params
+      @existing_ids = []
     end
 
     def recently_added
-      recently_added_products
+      products = recently_added_products
+      @existing_ids += products.map(&:id)
+      products
     end
 
     def most_popular
-      { products: most_popular_products, tags: most_popular_tags }
+      products = most_popular_products
+      @existing_ids += products.map(&:id)
+      { products: products, tags: most_popular_tags }
     end
 
     private
 
     def recently_added_products
       params = pagination_params(@params['recently_added'], DEFAULTS[RECENTLY_ADDED_SECTION])
-      Product.recently_added.limit(params[:limit]).offset(params[:offset])
+      Product.where.not(id: @existing_ids).recently_added.limit(params[:limit]).offset(params[:offset])
     end
 
     def most_popular_tags
@@ -37,7 +42,7 @@ module Fletcher
 
     def most_popular_products
       params = pagination_params(@params['most_popular'].try(:[], 'products'), DEFAULTS[MOST_POPULAR_SECTION][:products])
-      Product.most_popular.limit(params[:limit]).offset(params[:offset])
+      Product.where.not(id: @existing_ids).limit(params[:limit]).offset(params[:offset])
     end
 
     def pagination_params(params, defaults)
