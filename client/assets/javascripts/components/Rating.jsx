2db23aa42d3e55f0308aeb0bd742249bbded92f0
Halisson Vitor <halissonvit@gmail.com>
2015-08-06 22:56:56 -0300

    feat(client) add styling to enabled rating systems

--- client/assets/javascripts/components/Rating.jsx
+++ client/assets/javascripts/components/Rating.jsx
@@ -2,6 +2,54 @@ import React from 'react';
 import _ from 'lodash';
 
 class Rating extends React.Component {
+  _getAllStars() {
+    return $(React.findDOMNode(this.refs.container)).find('.rating-group');
+  }
+
+  _getPreviousStars(currentStar) {
+    return this._getAllStars(currentStar).filter(function( index ) {
+      let label = $(this).find('label');
+      let input = $(this).find('input.rating-item');
+      return input.val() < currentStar.find('input.rating-item').val();
+    })
+  }
+
+  _getCurrentStar(label) {
+    return $(label).parent('.rating-group');
+  }
+
+  _onMouseOver(e) {
+    if (!this.props.ratingEnabled)
+      return
+
+    let $label = $(e.target)
+
+    this._getAllStars().find('label').removeClass('hover');
+    this._getPreviousStars(this._getCurrentStar($label)).find('label').addClass('hover');
+    $label.addClass('hover')
+  }
+
+  _onMouseOut(e) {
+    if (!this.props.ratingEnabled)
+      return
+
+    let $label = $(e.target)
+
+    this._getAllStars().find('label').removeClass('hover');
+  }
+
+  _onClick(e) {
+    if (!this.props.ratingEnabled)
+      return
+
+    let $label = $(e.target)
+    let $star = this._getCurrentStar($label);
+
+    this._getAllStars().find('label').removeClass('marked').removeClass('before-marked');
+    this._getPreviousStars($star).find('label').addClass('before-marked');
+    $label.addClass('marked');
+  }
+
   buildStar(starValue) {
     let name = this.props.name;
     let id = this.props.id ? this.props.id : `${name}_${starValue}_${_.random(1, 99999)}`;
@@ -13,7 +61,9 @@ class Rating extends React.Component {
     return (<div className='rating-group'>
       <input className='rating-item' type='radio' id={id} name={name} value={starValue}
              disabled={!ratingEnabled} checked={checked}/>
-      <label htmlFor={id} className={marked ? 'marked' : ''}></label>
+           <label htmlFor={id} className={marked ? 'marked' : ''}
+             onMouseOver={this._onMouseOver.bind(this)} onMouseOut={this._onMouseOut.bind(this)}
+             onClick={this._onClick.bind(this)} ></label>
     </div>);
   }
 
@@ -22,12 +72,15 @@ class Rating extends React.Component {
     let max = this.props.max;
     let containerClass = this.props.containerClass;
     let containerClasses = _.compact(['rating-container', containerClass]).join(' ');
+    if (this.props.ratingEnabled) { containerClasses = _.compact([containerClasses, 'rating-enabled']).join(' ') }
     let rating = _.times(max, function buildStars(n) {
       return _this.buildStar(n + 1);
     });
 
-    return (<div className={containerClasses}>
-      {rating}
+    return (<div className={containerClasses} ref='container'>
+      <div className='items'>
+        {rating}
+      </div>
     </div>);
   }
 }
