36b4442ffa4adacc52fc6b5a5c5789df54f5db17
Halisson Vitor <halissonvit@gmail.com>
2015-08-10 19:33:39 -0300

    fix(api) fix search api to accept blank query param

--- app/services/fletcher/search.rb
+++ app/services/fletcher/search.rb
@@ -2,7 +2,7 @@ module Fletcher
   class Search
 
     def initialize(search_string)
-      @terms = get_terms(search_string)
+      @terms = get_terms(search_string || '')
     end
 
     def results
@@ -16,22 +16,23 @@ module Fletcher
     private
 
     def companies(terms)
-      Company.where{ (name.like_any(terms)) | (description.like_any(terms)) }
+      Company.where{ (name.like_any(terms)) | (description.like_any(terms)) } || []
     end
 
     def products(terms)
-      Product.where{ (name.like_any(terms)) | (description.like_any(terms)) }
+      Product.where{ (name.like_any(terms)) | (description.like_any(terms)) } || []
     end
 
     def tags(terms)
-      Tag.where{ (name.like_any(terms)) }
+      Tag.where{ (name.like_any(terms)) } || []
     end
 
     # We're going to take the search string the user input, split it up into words and then
     # search for each of those words anywhere in the targetted attributes. To do that we need to
     # prepend and append each word with % to show that it can appear anywhere in the searched string.
     def get_terms(search_string)
-      search_string.split(' ').each { |s| s.prepend('%').concat('%') }
+      terms = search_string.split(' ').each { |s| s.prepend('%').concat('%') }
+      terms.empty? ? ['%%'] : terms
     end
   end
 end
