69e12bc7111f9be48074536d0d2f8cad133da08b
Halisson Vitor <halissonvit@gmail.com>
2015-08-09 23:51:45 -0300

    fix(api) minor refactoring

--- app/models/concerns/has_avatar.rb
+++ app/models/concerns/has_avatar.rb
@@ -7,9 +7,10 @@ module HasAvatar
                       :path => "uploads/:instance_uuid/:style/:instance_uuid:basename.:extension"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+    private
+
     def ensure_avatar_uuid_has_a_value
       self.avatar_uuid = "%s%s" % [SecureRandom.uuid, self.avatar_file_size] if !self.avatar_uuid && self.avatar_file_size
     end
   end
-
 end
