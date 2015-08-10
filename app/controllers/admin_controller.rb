8d2fab4a207674ba518a034757b010292add1628
Kaio Magalhães <contato@kaiomagalhaes.com.br>
2015-08-10 20:17:33 -0300

    Revert "feat(admin) create the admin controller"
    This reverts commit 4d98f74d40af4e67782b2f772ff4d9c7364b9fd7.

--- app/controllers/admin_controller.rb
+++
@@ -1,8 +0,0 @@
-class AdminController < ApplicationController
-  def authenticate_active_admin_user!
-    unless current_user.admin?
-      flash[:alert] = "Unauthorized Access!"
-      redirect_to root_path
-    end
-  end
-end
