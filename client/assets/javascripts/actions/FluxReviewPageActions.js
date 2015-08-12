4f2f54afc08e2c42a34b1ed8fe105be256d26745
Halisson Vitor <halissonvit@gmail.com>
2015-08-12 19:53:29 -0300

    refactor(cli) move s3 upload file to an specific API file

--- client/assets/javascripts/actions/FluxReviewPageActions.js
+++ client/assets/javascripts/actions/FluxReviewPageActions.js
@@ -3,40 +3,7 @@ import alt from '../FluxAlt';
 import { Router, Navigation } from 'react-router'
 import NewReviewPageAPI from '../utils/NewReviewPageAPI';
 import ProductAPI from '../utils/ProductAPI';
-
-//TODO: refactor to another file
-function uploadFileToS3(file, uploadUrl, contentType, callbacks) {
-  var deferred = $.Deferred();
-
-  var xhr = new XMLHttpRequest();
-  xhr.open('PUT', uploadUrl, true);
-  xhr.setRequestHeader('Content-Type', contentType);
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      callbacks.onProgress(file, file.size);
-      deferred.resolve(uploadUrl.split('?')[0]);
-    } else {
-      deferred.reject(xhr);
-      callbacks.error(xhr);
-    }
-  };
-
-  xhr.onerror = function() {
-    callbacks.error(xhr);
-    deferred.reject(xhr);
-  };
-
-  xhr.upload.onprogress = function(e) {
-    if (e.lengthComputable) {
-      callbacks.onProgress(file, e.loaded);
-    }
-  };
-
-  xhr.send(file);
-
-  return deferred.promise();
-}
+import S3API from '../utils/S3API';
 
 class FluxReviewPageActions {
   updateReview(review) {
