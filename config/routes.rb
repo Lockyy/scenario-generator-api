7c6b84dab6e13aa0da0ff78a4799b0b4eb4703dc
Halisson Vitor <halissonvit@gmail.com>
2015-08-14 17:50:31 -0300

    api(user) expose api to fetch current_user data

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :companies, defaults: {format: 'json'}
 
     get 'search', to: 'search#index', as: 'search', defaults: {format: :json}
+    get 'users/current', defaults: {format: :json}
   end
 
   devise_for :users, only: [:omniauth_callbacks], controllers: {omniauth_callbacks: 'omniauth_callbacks'}
