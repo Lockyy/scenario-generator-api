2537fb96233f63d7e4ac2ee42da9732e3cde0dc0
Kaio Magalhães <contato@kaiomagalhaes.com.br>
2015-08-19 14:56:56 -0300

    feat(api) create searchable_by_name

--- app/models/concerns/searchable_by_name_and_description.rb
+++ app/models/concerns/searchable_by_name_and_description.rb
@@ -1,24 +1,21 @@
 module SearchableByNameAndDescription
   extend ActiveSupport::Concern
+  include SearchableByName
+
   included do
-    include PgSearch
-
-    pg_search_scope :search_by_name_and_description, :against => [
-                                                       [:name, 'A'],
-                                                       [:description, 'B']
-                                                   ], :using => {
-                                                       :tsearch => {:any_word => true, :prefix => true},
-                                                       :dmetaphone => {:any_word => true, :sort_only => true}
-                                                   }
-
-    pg_search_scope :search_by_name, :against => :name, :using => {
-                                       :tsearch => {:any_word => true, :prefix => true},
-                                       :dmetaphone => {:any_word => true, :sort_only => true}
-                                   }
+    pg_search_scope :search_by_name_and_description,
+      :against => [
+        [:name, 'A'],
+        [:description, 'B']
+      ],
+      :using => {
+        :tsearch => {:any_word => true, :prefix => true},
+        :dmetaphone => {:any_word => true, :sort_only => true}
+      }
 
     pg_search_scope :search_by_description, :against => :description, :using => {
-                                              :tsearch => {:any_word => true, :prefix => true},
-                                              :dmetaphone => {:any_word => true, :sort_only => true}
-                                          }
+      :tsearch => {:any_word => true, :prefix => true},
+      :dmetaphone => {:any_word => true, :sort_only => true}
+    }
   end
 end
