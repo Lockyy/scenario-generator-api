9e4efdf45cd62f89fcfa205340d8f546ade74936
Kaio Magalhães <contato@kaiomagalhaes.com.br>
2015-08-19 15:46:41 -0300

    feat(api) change convert_tags_taggable migration to create tag_taggable based on the tag

--- db/migrate/20150814163348_convert_tags_taggable_relationship_to_many_to_many.rb
+++ db/migrate/20150814163348_convert_tags_taggable_relationship_to_many_to_many.rb
@@ -3,12 +3,15 @@ class ConvertTagsTaggableRelationshipToManyToMany < ActiveRecord::Migration
     create_table :tag_taggables do |t|
       t.integer :tag_id
       t.integer :taggable_id
-      t.string  :taggable_type
+      t.string :taggable_type
 
       t.timestamps null: false
     end
 
-    Tag.all.destroy_all
+    Tag.all.each do |tag|
+      TagTaggable.create({taggable_id: tag.taggable_id, taggable_type: tag.taggable_type, tag_id: tag.id})
+    end
+
 
     remove_column :tags, :taggable_id, :integer
     remove_column :tags, :taggable_type, :string
