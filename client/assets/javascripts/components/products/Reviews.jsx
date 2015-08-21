9d5d7b059f4a28d36a224b8606c229232cf0a990
Kaio Magalhães <contato@kaiomagalhaes.com.br>
2015-08-21 20:58:22 -0300

    feature(cli) create view to vote on the reviews

--- client/assets/javascripts/components/products/Reviews.jsx
+++ client/assets/javascripts/components/products/Reviews.jsx
@@ -43,6 +43,15 @@ const Reviews = React.createClass({
     });
   },
 
+  voteOnReview: function(e){
+    let elementData = $(e.target).data();
+    let prodId = elementData.productId;
+    let revId = elementData.reviewId;
+    let helpful = elementData.helpful;
+
+    FluxProductReviewsActions.voteOnReview(prodId, revId, helpful);
+  },
+
   renderReview: function(review) {
     let attachments = _.collect(review.attachments, function(attachment) {
       return (<li className='attachment'>
@@ -55,6 +64,25 @@ const Reviews = React.createClass({
         <a className="link" href={UrlHelper.addProtocol(link.url)} target='_blank'>{UrlHelper.addProtocol(link.url)}</a>
       </li>);
     });
+    let wrotByCurrentUser = this.context.currentUser.id == review.user.id;
+
+    let editMyReview =  <div className='edit-review-container'>
+                          <Link to={`/app/products/${review.reviewable.id}/reviews/${review.id}`}
+                               className='btn btn-white btn-round'>Edit my review</Link>
+                        </div>;
+
+    let productId = review.product.id;
+    let reviewId = review.id;
+    let itWasHelpful = <div className='helpful-review-container'>
+                          <span className='helpful-reviews-text'> Was this review helpful to you?</span>
+                          <button className='btn btn-grey btn-round' data-product-id={productId} data-review-id={reviewId}
+                                  data-helpful='true' onClick={this.voteOnReview}> Yes </button>
+                          <button className='btn btn-grey btn-round' data-product-id={productId} data-review-id={reviewId}
+                                  data-helpful='false' onClick={this.voteOnReview}> No </button>
+                        </div>;
+
+    let userEditAction =   wrotByCurrentUser ? editMyReview
+      : itWasHelpful;
 
     return (
       <div className="row review">
@@ -100,12 +128,8 @@ const Reviews = React.createClass({
           </div>
           <div className="price-review" dangerouslySetInnerHTML={{__html: review.formatted_price_review}} />
           <Tags tags={review.tag_list} />
-          <div className='edit-review-container'>
-            {this.context.currentUser.id == review.user.id ?
-              (<Link to={`/app/products/${review.reviewable.id}/reviews/${review.id}`}
-                className='btn btn-white btn-round'>Edit my review</Link>) : ''
-            }
-          </div>
+
+          {userEditAction}
         </div>
       </div>
     )
