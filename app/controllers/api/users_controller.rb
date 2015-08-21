d6e7ffec5655392e59cc25bb817d725b134b65cd
Halisson Vitor <halissonvit@gmail.com>
2015-08-21 17:13:00 -0300

    feat(api) expose get users/id on api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,14 @@ class Api::UsersController < AppController
     @user = current_user
 
     respond_to do |format|
+      format.json { render :show }
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
       format.json { render }
     end
   end
