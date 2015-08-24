ae9f09a2992555290de5a0d58689c7abf8171474
Halisson Vitor <halissonvit@gmail.com>
2015-08-24 14:44:45 -0300

    fix(app) add verification to authenticate user with devise to avoid invalid sessions

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -2,6 +2,7 @@ class AppController < ApplicationController
   layout 'app'
 
   before_filter :authenticate!
+  before_filter :authenticate_user!
 
   def index
   end
--- app/helpers/welcome_message_helper.rb
+++ app/helpers/welcome_message_helper.rb
@@ -10,7 +10,7 @@ module WelcomeMessageHelper
   # The welcome message should be displayed everytime the user signs in and access the dashboard.
   def show_welcome_message?
     # Verifies if the cookie has already been saved and if it matches the sign_in_count
-    user_has_just_signed_in = cookies[:sign_in_count].to_s != current_user.sign_in_count.to_s
+    user_has_just_signed_in = cookies[:sign_in_count].to_s != current_user.try(:sign_in_count).to_s
     current_url = url_for
 
     current_url == app_path && user_has_just_signed_in
@@ -18,6 +18,6 @@ module WelcomeMessageHelper
 
   def save_welcome_message_state
     # Save the current_user sign in count to assign that he has just signed in
-    cookies[:sign_in_count] = current_user.sign_in_count
+    cookies[:sign_in_count] = current_user.try(:sign_in_count)
   end
 end
