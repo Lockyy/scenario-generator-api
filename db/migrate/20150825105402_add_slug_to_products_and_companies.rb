869259354856462ef9de6caebae067a5026edca2
Daniel Lockhart <daniel@lockyy.com>
2015-08-25 15:42:21 +0100

    bugfix(api) Generate slugs after migration

--- db/migrate/20150825105402_add_slug_to_products_and_companies.rb
+++ db/migrate/20150825105402_add_slug_to_products_and_companies.rb
@@ -4,5 +4,8 @@ class AddSlugToProductsAndCompanies < ActiveRecord::Migration
     add_column :companies, :slug, :string
     add_index :products, :slug
     add_index :companies, :slug
+
+    Product.find_each(&:save)
+    Company.find_each(&:save)
   end
 end
