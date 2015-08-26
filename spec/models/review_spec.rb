92457b839795bc0a8381d2e0e5fe64ce40a7eba4
Halisson Vitor <halissonvit@gmail.com>
2015-08-26 00:12:35 -0300

    fix(app) add constraint to not allow an user to create more than one review per product

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Review, type: :model do
 
   it { expect(subject).to have_many :attachments }
   it { expect(subject).to have_many :tags }
+
+  it { expect(subject).to validate_uniqueness_of(:user_id).scoped_to(:reviewable_id) }
 end
