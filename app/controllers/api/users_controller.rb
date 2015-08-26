bbea340ef5b8181d974cf55dad9edf26d4e76ca3
Halisson Vitor <halissonvit@gmail.com>
2015-08-25 22:00:05 -0300

    feat(cli) change route /user/current to always set the current user id

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,5 @@
 class Api::UsersController < AppController
-  def current
-    @user = current_user
-
-    respond_to do |format|
-      format.json { render :show }
-    end
-  end
+  before_filter :set_current_user_id
 
   def show
     @user = User.find(params[:id])
@@ -13,5 +7,19 @@ class Api::UsersController < AppController
     respond_to do |format|
       format.json { render }
     end
+  end
+
+  def recent_activity
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.json { render }
+    end
+  end
+
+  private
+
+  def set_current_user_id
+    params[:id] = current_user.id if params[:id] == 'current'
   end
 end
