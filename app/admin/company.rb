4dded9de2c39849c4fca01bb380e9b42cad3dff1
Halisson Vitor <halissonvit@gmail.com>
2015-08-28 06:04:58 -0300

    fix(admin) fix admin to work with friendly url

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -1,6 +1,16 @@
 ActiveAdmin.register Company do
   permit_params :name, :url, :description, :avatar
 
+  controller do
+    def find_resource
+      begin
+        scoped_collection.where(slug: params[:id]).first!
+      rescue ActiveRecord::RecordNotFound
+        scoped_collection.find(params[:id])
+      end
+    end
+  end
+
   form do |f|
     f.semantic_errors
     inputs 'Details' do
@@ -33,4 +43,3 @@ ActiveAdmin.register Company do
     end
   end
 end
-
