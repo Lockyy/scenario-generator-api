4e3a1237b7176edddc006f56f9f47795d447bf47
Cody Littlewood <cody@codelitt.com>
2015-09-04 12:40:15 -0400

    feature(app/tests) Finished User profile feature tests

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :review do
+    association :user
+    
     title { Faker::Company.bs }
-
+    quality_review { Faker::Company.catch_phrase }
+    quality_score { Faker::Number.between(1,5) }
+    price_review { Faker::Company.bs }
+    price_score { Faker::Number.between(1,5) }
   end
 end
