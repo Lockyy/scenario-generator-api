236748c1610c5bbf02686d49f0d5991cf46e2632
Daniel Lockhart <daniel@lockyy.com>
2015-09-04 14:52:10 +0100

    feature(app) Add bookmarks to sidebar

--- client/assets/javascripts/components/MyBookmarks.jsx
+++ client/assets/javascripts/components/MyBookmarks.jsx
@@ -12,23 +12,42 @@ const MyBookmarks = React.createClass ({
       showTitle: false,
       title: 'My bookmarks',
       showMessage: false,
-      message: 'Quick access to browse your favorite products or continue where you left off.'
+      message: 'Quick access to browse your favorite products or continue where you left off.',
+      size: '2-5',
+      sidebar: false
     }
+  },
+
+  products: function() {
+    return this.props.sidebar ? this.props.products.slice(0, 4) : this.props.products
+  },
+
+  boxSize: function() {
+    return this.props.sidebar ? '4-5' : this.props.size
+  },
+
+  renderShowMoreButton: function() {
+    return this.props.sidebar ? <a href='/app/users/current#bookmarks' className='link-view-all'>Show All Bookmarks</a> : ''
+  },
+
+  renderProductBox: function(product, boxSize) {
+    return ( <ProductBox size={boxSize} {...product} /> );
   },
 
   render: function() {
     let details = <div className={`message ${this.props.messageClass}`}>{this.props.message}</div>;
+    let _this = this
 
     return (
       <div className={`my-bookmarks ${this.props.className || ''}`}>
         {this.props.showTitle ? <h2>{this.props.title}</h2> : ''}
 
         {this.props.showMessage ? details : '' }
-        {_.map(this.props.products, function(product) {
-          return (
-            <ProductBox size={2} {...product} />
-          )
+        {_.map(this.products(), function(product) {
+          return _this.renderProductBox(product, _this.boxSize())
         })}
+
+        { this.renderShowMoreButton() }
 
       </div>
     )
