5b42e22d27b625804b650cc69cf6f8f98383c703
Cody Littlewood <cody@codelitt.com>
2015-09-06 11:41:45 -0400

    fix(tests) Finally got capybara to click review quality score.
    The problem is that react components are only slightly different
    fore the quality review score and the price rating. And where
    they are different seems to auto generate every time react is
    loaded with a slightly different label for=

--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -16,7 +16,7 @@ feature "Create reviews", js:true do
     fill_in "product[company[name]]", with: "Ubuntu"
     fill_in "product[url]", with: "http://www.ubuntu.com/phone"
     fill_in "product[description]", with: Faker::Lorem.paragraph 
-    find(:xpath, "//label[@for='product[review[quality_score]]_5_39105']")
+    first('#name_5').click
     fill_in "product[review[title]]", with: "Pretty damn cool phone"
     fill_in "product[review[quality_review]]", with: Faker::Lorem.paragraph
     find("value=create Review").click
