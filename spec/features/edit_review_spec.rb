9da2732bcb9a3d6331d98351474f183310b719d6
Daniel Lockhart <daniel@lockyy.com>
2015-09-15 19:23:28 +0100

    bugfix(tests) Fix how tests click

--- spec/features/edit_review_spec.rb
+++ spec/features/edit_review_spec.rb
@@ -18,8 +18,8 @@ describe "Edit review page", js:true do
     describe "clearing the price_score" do
       it 'sets the reviews price_score to nil' do
         expect(@review.price_score).to_not eq nil
-        find('.price_score.rating-container .clear-button').click
-        find(".btn.btn-default.submit.btn-round").click
+        find('.price_score.rating-container .clear-button').trigger('click')
+        find(".btn.btn-default.submit.btn-round").trigger('click')
         wait_for_ajax
         expect(@review.price_score).to eq nil
       end
@@ -28,8 +28,8 @@ describe "Edit review page", js:true do
     describe "clearing the quality_score" do
       it 'sets the reviews quality_score to nil' do
         expect(@review.quality_score).to_not eq nil
-        find('.quality_score.rating-container .clear-button').click
-        find(".btn.btn-default.submit.btn-round").click
+        find('.quality_score.rating-container .clear-button').trigger('click')
+        find(".btn.btn-default.submit.btn-round").trigger('click')
         wait_for_ajax
         sleep(5000)
         expect(@review.quality_score).to eq nil
