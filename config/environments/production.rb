511e649099890923fbc2047af53f086d89335c5d
Halisson Vitor <halissonvit@gmail.com>
2015-09-21 17:13:32 -0300

    fix(app) remove basic auth for prod. environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.middleware.use '::Rack::Auth::Basic' do |u, p|
-    [u, p] == ['fletcher', 'newtools']
-  end
 end
