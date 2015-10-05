aa19a7921b6b886e65c53e1372d71b99d461c014
Daniel Lockhart <daniel@lockyy.com>
2015-10-05 21:17:04 +0100

    feature(site) Change email destination for support form submissions

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -2,7 +2,7 @@ class SupportMailer < ApplicationMailer
 
   def submission(params)
     @params = params
-    mail( to: 'fletcher@am.jll.com',
+    mail( to: SUPPORT_EMAIL,
           subject: 'Support Submission',
           reply_to: params[:email] )
   end
