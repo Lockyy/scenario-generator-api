86d7b6164344a3edacb9bc12c35e611f34aa7158
Daniel Lockhart <daniel@lockyy.com>
2015-12-03 12:39:03 +0000

    bugfix(app) Add owner of collection to all CollectionProducts during migration

--- db/migrate/20151130123245_add_user_id_to_collection_products.rb
+++ db/migrate/20151130123245_add_user_id_to_collection_products.rb
@@ -1,5 +1,9 @@
 class AddUserIdToCollectionProducts < ActiveRecord::Migration
   def change
     add_column :collection_products, :user_id, :integer
+    CollectionProduct.all.each do |collection_product|
+      collection_product.user_id = collection_product.collection.user.id
+      collection_product.save
+    end
   end
 end
