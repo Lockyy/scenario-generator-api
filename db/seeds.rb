1bbeb9cfe4b95164885122d1ddda66c3e5e522ea
Daniel Lockhart <daniel@lockyy.com>
2015-12-03 22:04:47 +0000

    feature(db) Add seeds for collections

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 def create_users
   30.times do
-    User.create(name: Faker::Name.name,
-                job_title: Faker::Company.bs,
-                avatar_url: Faker::Company.logo,
-                email: Faker::Internet.safe_email,
-                password: 'password')
+    user = User.create( name: Faker::Name.name,
+                        job_title: Faker::Company.bs,
+                        avatar_url: Faker::Company.logo,
+                        email: Faker::Internet.safe_email,
+                        password: 'password')
+    puts "Creating User: #{user.name}"
   end
 
   User.all
@@ -14,6 +15,7 @@ def create_product(users)
   product = Product.create( name: Faker::Company.bs,
                             description: 10.times.collect { Faker::Company.bs }.join(' '),
                             company: Company.new(name: Faker::Company.name))
+  puts "Creating Product: #{product.name}"
 
   users.each do |user|
     review = product.reviews.create( title: Faker::Company.bs,
@@ -22,6 +24,7 @@ def create_product(users)
                             price_score: rand(5) + 1,
                             quality_score: rand(5) + 1,
                             user: user)
+    puts "Creating Review: #{review.title} by #{user.name}"
   end
 end
 
@@ -31,6 +34,23 @@ def create_tags(reviews)
     reviews.each do |review|
       TagTaggable.create(tag: tag, taggable: review)
     end
+    puts "Creating Tag: #{tag.name}"
+  end
+end
+
+def create_collection(user, products, privacy, shares = 0)
+  collection = Collection.create(name: Faker::Company.bs, description: Faker::Company.bs, user: user, products: products, privacy: privacy)
+  puts "Creating Collection: #{collection.name}"
+  if(privacy == 'hidden')
+    share_array = []
+    (1..shares).to_a.each do |share|
+      user = User.all.sample(1)[0]
+      rank = rand(3)
+      user_info = {id: user.id, rank: rank}
+      share_array.push user_info
+      puts "Sharing Collection with #{user.name} at rank #{rank}"
+    end
+    collection.share(share_array)
   end
 end
 
@@ -42,4 +62,9 @@ end
 
 25.times do
   create_tags(Review.all.sample(2))
+end
+
+50.times do
+  create_collection(users.sample(1)[0], Product.all.sample(2), 'visible')
+  create_collection(users.sample(1)[0], Product.all.sample(2), 'hidden', 5)
 end
