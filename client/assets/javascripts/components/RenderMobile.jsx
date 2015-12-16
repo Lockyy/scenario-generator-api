954d6d97c3fe7d19de62ef063f7ac9cad34fed96
Daniel Lockhart <daniel@lockyy.com>
2015-12-16 16:00:11 +0000

    feature(app) Allow user to pass in a conditional to RenderMobile and RenderDesktop
    This can be used for deciding whether to render or not. Won't be needed in 99% of cases.

--- client/assets/javascripts/components/RenderMobile.jsx
+++ client/assets/javascripts/components/RenderMobile.jsx
@@ -4,12 +4,25 @@ import MediaQuery from 'react-responsive';
 import MediaQueryConstants from '../utils/constants/MediaQueryConstants';
 
 const RenderMobile = React.createClass ({
+
+  getDefaultProps: function() {
+    return {
+      conditional: true
+    }
+  },
+
   render: function() {
-    return (
-      <MediaQuery
-        query={MediaQueryConstants.MOBILE}
-        {... this.props} />
-    )
+    if(this.props.conditional) {
+      return (
+        <MediaQuery
+          query={MediaQueryConstants.MOBILE}
+          {... this.props} />
+      )
+    } else {
+      return (
+        <div/>
+      )
+    }
   },
 })
 
