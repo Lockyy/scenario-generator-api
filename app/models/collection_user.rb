be29c8d4ef4cbe10f688dd6691f495f1750467a1
Daniel Lockhart <daniel@lockyy.com>
2015-12-17 19:55:24 +0000

    bugfix(api) Collections returned via the API now use updated_at instead of created_at for display_date

--- app/models/collection_user.rb
+++ app/models/collection_user.rb
@@ -1,6 +1,6 @@
 class CollectionUser < ActiveRecord::Base
 
-  belongs_to :shared_collection, class_name: 'Collection', foreign_key: 'collection_id'
+  belongs_to :shared_collection, class_name: 'Collection', foreign_key: 'collection_id', touch: true
   belongs_to :sharee, class_name: 'User', foreign_key: 'sharee_id'
 
   after_create :create_notification
