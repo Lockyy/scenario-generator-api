43973b0ec69d96375bc49b6158ca857791658da4
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-12-17 11:22:13 -0200

    change delete collection event to redirect to user collections after it done

--- client/assets/javascripts/components/collections/CollectionPage.jsx
+++ client/assets/javascripts/components/collections/CollectionPage.jsx
@@ -97,9 +97,10 @@ const CollectionPage = React.createClass({
         FluxCollectionActions.deleteCollection({
           id: _this.props.params.id,
           name: collection_name
-        })
-        let previous = _this.getParameterByName('link') || `/app/users/current`
-        _this.context.router.transitionTo(previous)
+        });
+        //TODO: Change this into a transition
+        let previous = _this.getParameterByName('link') || `/app/users/current#collections`;
+        window.location.href = previous
       }
     })
   },
