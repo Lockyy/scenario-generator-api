4b0ae8c4347e0bea803d45944ee2de4776d14bbf
Daniel Lockhart <daniel@lockyy.com>
2015-12-23 16:21:55 +0000

    bugfix(app) Fix whitelisting of users
    - Add whitelisting checks at the omniauth level
    - Ensure all emails being checked are downcased

--- app/models/allowed_user.rb
+++ app/models/allowed_user.rb
@@ -1,2 +1,12 @@
 class AllowedUser < ActiveRecord::Base
+
+  before_save :downcase_email
+
+  def self.whitelisted? email
+    !self.find_by(email: email).nil?
+  end
+
+  def downcase_email
+    self.email = self.email.downcase
+  end
 end
