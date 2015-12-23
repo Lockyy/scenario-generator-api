f3de8a89a2ea278893801852b10b94e9c8777b4e
Daniel Lockhart <daniel@lockyy.com>
2015-12-23 17:19:18 +0000

    bugfix(api) Fix whitelisting on AllowedUser

--- app/models/allowed_user.rb
+++ app/models/allowed_user.rb
@@ -3,6 +3,7 @@ class AllowedUser < ActiveRecord::Base
   before_save :downcase_email
 
   def self.whitelisted? email
+    return true unless ENV['ENABLE_WHITELIST']
     !self.find_by(email: email).nil?
   end
 
