1155d5123484681d9487c84999aebcdf5836a852
Daniel Lockhart <daniel@lockyy.com>
2016-01-14 13:05:57 +0000

    bugfix(tests) Remove threading from notifications system

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -5,12 +5,7 @@ module Api
       @notification_ids = current_user.notifications.unviewed.map(&:id)
       @notifications = Notification.where(id: @notification_ids)
 
-      Thread.start do
-        Notification.mark_viewed(@notification_ids)
-        if (ActiveRecord::Base.connection && ActiveRecord::Base.connection.active?)
-           ActiveRecord::Base.connection.close
-        end
-      end
+      Notification.mark_viewed(@notification_ids)
 
       respond_to do |format|
         format.json { render }
