8f0ef978bb9640aafb40b056b22fc0d269ea608d
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-01-27 22:43:17 -0200

    change media query base for desktop and mobile on media query constants

--- client/assets/javascripts/utils/constants/MediaQueryConstants.js
+++ client/assets/javascripts/utils/constants/MediaQueryConstants.js
@@ -3,6 +3,6 @@ module.exports = {
   SM: '(min-device-width: 768px) and (max-device-width: 992px)',
   MD: '(min-device-width: 992px) and (max-device-width: 1200px)',
   LG: '(min-device-width: 1200px)',
-  DESKTOP: '(min-device-width: 768px)',
-  MOBILE: '(max-device-width: 768px)'
+  DESKTOP: '(min-width: 768px)',
+  MOBILE: '(max-width: 768px)'
 };
