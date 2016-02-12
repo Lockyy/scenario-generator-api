b5f4a3856500793ead5b0a0d76c07815fde1f605
Daniel Lockhart <daniel@lockyy.com>
2016-02-12 19:23:51 +0000

    bugfix(api) Fix attachment url signing expirating time

--- app/services/fletcher/assets/signer.rb
+++ app/services/fletcher/assets/signer.rb
@@ -3,11 +3,15 @@ class Fletcher::Assets::Signer
     @signer = signer
   end
 
+  # Expires in is set to 120, which is two minutes because for
+  # some reason the urls generated expire 100 seconds earlier than
+  # the time we put in. Which means 120 seconds will actually only
+  # last for 20 seconds.
   def sign_url(path)
     @signer.presigned_url(:get_object,
                           bucket: ENV['S3_BUCKET_NAME'],
                           key: path,
-                          expires_in: 3,
+                          expires_in: 120,
                           secure: true)
   end
 end
