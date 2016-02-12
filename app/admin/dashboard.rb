35fdc79967ab0ca7e9aa0908d1f44feeb1ef790b
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-02-12 12:41:17 -0200

    add application version on admin session

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,6 +1,9 @@
 ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 0, label: proc{
+    "Application version: #{Rails.application.config.version}"
+  }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     columns do
